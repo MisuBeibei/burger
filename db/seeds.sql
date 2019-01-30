2a4e75cd759820427118a347702f3171a807f5e2
unknown <39747964+MisuBeibei@users.noreply.github.com>
2019-01-29 23:57:17 -0800

    build up the whole structure

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Triple-Stacked Avocado Burger");
+INSERT INTO burgers (burger_name) VALUES ("Blue Cheese Bison Burger");
+INSERT INTO burgers (burger_name) VALUES ("Double Mushroom Burger");
